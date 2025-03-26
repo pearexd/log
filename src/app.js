efc77a2becfa8b61009e68b2bfcbced4864098bc
pearexd <work.bhargav.tech@gmail.com>
2025-03-26 08:03:32 +0530

    Pre Prod Fixes

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ const rateLimiter = rateLimit({
 app.use(rateLimiter)
 
 app.use(cookieParser())
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({extended:true,limit:'50mb'}))
+app.use(express.json({limit:'50mb'}))
 app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true
